f36c1a72624e2e61be8c20691aab39d6b966de11
João Bertholino <joaobertholino964@gmail.com>
2024-07-21 12:28:27 -0300

    Spring project template.

---
+++ src/main/java/com/joaobertholino/udemyspringproject/UdemyspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.joaobertholino.udemyspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaobertholino/udemyspringproject/UdemyspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobertholino.udemyspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
