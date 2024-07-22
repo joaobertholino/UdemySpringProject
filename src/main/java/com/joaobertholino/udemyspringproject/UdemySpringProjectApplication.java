01f8533715b3e77b86db08872917e9935b73e358
João Bertholino <joaobertholino964@gmail.com>
2024-07-22 11:11:20 -0300

    Subindo criação de perfil "Test" juntamente com as dependencias do Spring Data e banco de dados H2, juntamente com ajustes de indentação.

--- src/main/java/com/joaobertholino/udemyspringproject/UdemySpringProjectApplication.java
+++ src/main/java/com/joaobertholino/udemyspringproject/UdemySpringProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UdemyspringprojectApplication {
+public class UdemySpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UdemyspringprojectApplication.class, args);
+		SpringApplication.run(UdemySpringProjectApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/joaobertholino/udemyspringproject/config/TestConfig.java
@@ -0,0 +1,37 @@
+package com.joaobertholino.udemyspringproject.config;
+
+import com.joaobertholino.udemyspringproject.entities.Order;
+import com.joaobertholino.udemyspringproject.entities.User;
+import com.joaobertholino.udemyspringproject.repositories.OrderRepository;
+import com.joaobertholino.udemyspringproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.time.Instant;
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User("Joao", "joao@gmail.com", "99999999999", "98989898");
+		User u2 = new User("Ellen", "ellen@gmail.com", "88888888888", "76767676");
+
+		Order o1 = new Order(Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(Instant.parse("2019-07-12T07:34:15Z"), u1);
+
+		this.userRepository.saveAll(Arrays.asList(u1, u2));
+		this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+	}
+}
